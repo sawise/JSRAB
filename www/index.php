916e94cd9c0433fe2035d3d8778a71d31d26f5ba
Sam Johnsson <johnsson.sam@gmail.com>
2013-11-25 13:10:25 +0100

    searchpage layout is done, the logo is there

---
+++ www/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="css/style.css" type="text/css">
+  <link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+  </head>
+  <body><img src="img/logo.png" class="logo">
+<div class="container centered mainDiv">
+	<div class="row-fluid">
+	<div class="span12">
+	  <div class="searchInput">
+	         <form>
+	         <input type="text" placeholder="" class="input-xxlarge search-query">
+			    <button type="submit" class="btn">Search</button>
+			
+		</form>
+		</div>
+	      <div class="searchResult"><table class="table table-striped">
+	 		<thead><th>Ordernummer<th>Företag/Kund<th>Mönster<th><th>Dimension</th><th>Antal</th><th></th></thead>
+	 		 <tbody>
+	            <tr>
+	                <td></td>
+	                <td></td>
+	                <td></td>
+	                <td></td>
+	                <td></td>
+	                <td></td>
+	                <td><a href="#" class="btn btn-mini btn-primary btn-Action" type="button">Mer info</button></td>
+	               
+	 			</tr>
+			</tbody>
+	 		</table>
+		</div>
+		</div>
+	    </div>
+  </div>
+</div>
+  
+  </body>
+</html>
