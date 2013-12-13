4326c0be3622cb11e5cfe0a1ae1e49164a98f19d
Sam Johnsson <johnsson.sam@gmail.com>
2013-12-13 12:38:06 +0100

    Create order is working

--- www/addorder.php
+++ www/addorder.php
@@ -3,8 +3,7 @@
 require_once('../config.php');
    $tireTreads = $db->getTiretreads();
    $tireSize = $db->getTiresize();
-   $toggleThrArray = array('tirethreadText-', 'tirethread'); 
-   $toggleDimArray = array('dimensionText-', 'dimension'); 
+   $customers = $db->getCustomers();
  ?>
 <script>
   $(function() {
@@ -13,30 +12,69 @@ require_once('../config.php');
       firstDay: 1
     });
   });
-  
+
+  $(function() {
+	    var tireTreads = [
+	    <?php for ($i = 0; $i < count($tireTreads); $i++){
+	    	echo '"'.$tireTreads[$i]->id.'->'.$tireTreads[$i]->name.'"';
+	    	if($i < count($tireTreads)-1){
+	    		echo ',';
+	    	}
+	    } ?>
+	    ];
+
+	    var customers = [
+	    <?php for ($i = 0; $i < count($customers); $i++){
+	    	echo '"'.$customers[$i]->id.'->'.$customers[$i]->name.'"';
+	    	if($i < count($customers)-1){
+	    		echo ',';
+	    	}
+	    } ?>
+	    ];
+
+	    var tireSize = [
+	    <?php for ($i = 0; $i < count($tireSize); $i++){
+	    	echo '"'.$tireSize[$i]->id.'->'.$tireSize[$i]->name.'"';
+	    	if($i < count($tireSize)-1){
+	    		echo ',';
+	    	}
+	    } ?>
+	    ];
+
+ $( "#customer" ).autocomplete({
+      source: customers
+    });
+
+    $( "#dimension" ).autocomplete({
+      source: tireSize
+    });
+
+    $( "#tirethreads" ).autocomplete({
+      source: tireTreads
+    });
+  });
+
 $(function() {
     $( document ).tooltip();
   });
 
-$(document).ready(function(){
-  var tireText = document.getElementById('tirethreadText-');
-   var dimText = document.getElementById('dimensionText-');
-	tireText.style.display = 'none';
-	dimText.style.display = 'none';
-});
+//$(document).ready(function(){
+  //var tireText = document.getElementById('tirethreadText-');
+   //var dimText = document.getElementById('dimensionText-');
+	//tireText.style.display = 'none';
+	//dimText.style.display = 'none';
+//});
 
 
 
 
   </script> 
- <form class="form-horizontal"  method="post" action="">
+ <form class="form-horizontal" method="post" action="createOrder.php">
  	 <fieldset>
  		 <?php echo form_input('text', 'datepicker', 'Leveransdatum:', 'Tryck här för att välja datum') ?>
-		 <?php echo form_input('text', 'company', 'Kund:', 'Kundnamn') ?>
-		 <?php echo form_select('tirethread', 'Mönster: ', $tireTreads, null, 'thread', 'toggle', $toggleThrArray) ?>
-		  <?php echo form_input('text', 'tirethreadText', 'Mönster:', 'Skriv in däckmönstret här', '', 'untoggle', $toggleThrArray) ?>
-		 <?php echo form_select('dimension', 'Dimension: ', $tireSize, null, 'size','toggle', $toggleDimArray) ?>
-		 <?php echo form_input('text', 'dimensionText', 'Dimension:', 'Skriv in dimensionen här', '', 'untoggle', $toggleDimArray) ?>
+		 <?php echo form_input('text', 'customer', 'Kund:', 'Kundnamn') ?>
+		  <?php echo form_input('text', 'dimension', 'Dimension:', 'Skriv in däckmönstret här', '') ?>
+		 <?php echo form_input('text', 'tirethreads', 'Mönster:', 'Skriv in mönster här', '') ?>
 		 <?php echo form_input('text', 'total', 'Antal:','ex. 1' ) ?>
 		 <?php echo text_area('notes', 'Kommentar: ', 'Ordernummer, följenummer mm'); ?>
 		 <?php echo submit_button("Spara") ?>
---
+++ www/createOrder.php
@@ -0,0 +1,55 @@
+<?php
+
+  require_once('../config.php');
+
+  $db = new Db();
+
+  $tiresizeID = '';
+  $tiretreadID = '';
+  $customerID = '';
+
+  $deliverydate = $_POST['datepicker'];
+  $customer = $_POST['customer'];
+  $dimension = $_POST['dimension'];
+  $tirethread = $_POST['tirethreads'];
+  $total = $_POST['total'];
+  $notes = $_POST['notes'];
+  $now = date('m/d/y');
+
+	if (strpos($tirethread,'->')) {
+		$arrayslice = explode('->',$tirethread);
+		$tiretreadID = $arrayslice[0];
+		echo $tiretreadID;
+	} else {
+		$tiretreadID = $db->createTiretread($tirethread);
+	}
+
+	if (strpos($dimension,'->')) {
+		$arrayslice = explode('->',$dimension);
+		$tiresizeID = $arrayslice[0];
+		echo $tiresizeID;
+	} else {
+		$tiresizeID = $db->createTireSize($dimension);
+	}
+	if (strpos($customer,'->')) {
+		$arrayslice = explode('->',$customer);
+		$customerID = $arrayslice[0];
+		echo $customerID;
+	} else {
+		$customerID = $db->createCustomer($customer, 0000);
+	}
+
+	$orderID = $db->createOrder($customerID, $tiretreadID, $tiresizeID, $total, $notes, $deliverydate, 1 ,$now);
+	if($orderID){
+		
+		set_feedback('success', 'Ordern skapades, gå in i sök för att hitta den');
+		header('index.php');
+	} else{
+		set_feedback('error', 'Något blev galet, försök igen');
+	}
+
+
+
+
+ 
+
